97df15a2f780450b0d5500af4857c9c08ff58248
joseCarlosAc <jose.acevesgon@gmail.com>
2024-11-05 19:33:30 -0600

    feat: add internal server error message to response codes

--- src/types/response-codes.ts
+++ src/types/response-codes.ts
@@ -6,6 +6,7 @@ enum ResponseCodes {
 	FORBIDDEN = 403,
 	NOT_FOUND = 404,
 	INTERNAL_SERVER_ERROR = 500,
+	INTERNAL_SERVER_ERROR_MESSAGE = 'Internal Server Error: Something went wrong on our end. Please try again later or contact support if the issue persists.',
 }
 
 export default ResponseCodes;
