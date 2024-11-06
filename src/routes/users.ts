b7f400820332e92f8282fd0a50f15b14ea886b32
joseCarlosAc <jose.acevesgon@gmail.com>
2024-11-05 18:43:16 -0600

    finish of migration of routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,9 +5,19 @@ const router = Router();
 //GET para obtener todos los usuarios
 router.get('/', (req, res) => {});
 
-//GET para mostrat los mejores
+//GET para mostrar los mejores boards de un usuario
 router.get('/bestScores', (req, res) => {});
+
+//PUT para modificar los mejores boards de un usuario
+router.put('/bestScores', (req, res) => {});
+
+//GET para obtener los juegos guardados de un usuario
 router.get('saveGames', (req, res) => {});
+
+//PUT para modificar los juegos guardados de un usuario
+router.put('saveGames', (req, res) => {});
+
+//GET para obtener los usuarios que han jugado
 router.get('leaders', (req, res) => {});
 
 //PUT para modificar usuarios
